7214479e37c85194d90c599dbff299ce27d1016a
unknown <lucas.silva2000timao@gmail.com>
2023-09-21 19:30:29 -0300

    dpdf

---
+++ index.js
@@ -0,0 +1,27 @@
+var express = require("express");
+var app = express();
+var cors = require('cors');
+
+app.use(cors());
+
+app.get("/", (req, res) => {
+    res.json({message: "Requisição realizada com sucesso!"})
+});
+
+app.listen(3000, () => {
+    console.log("servidor rodando na porta 3000")
+});
+
+app.get("/download", (req, res) => {
+    var path =require("path");
+    var file = path.join(__dirname, "./file.pdf");
+
+    res.download(file, function (err){
+        if (err) {
+            console.log("Error");
+            consloe.log(err);
+        } else { 
+            console.log("Sucesso");
+        }
+    });
+});
